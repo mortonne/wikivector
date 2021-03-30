9010be475fd2f51d294c9495d44cc8581531b349
Neal Morton <mortonne@gmail.com>
2021-03-29 23:07:10 -0500

    build: change to using config for most setup settings

--- setup.py
+++ setup.py
@@ -1,36 +1,5 @@
 import glob
 import setuptools
 
-
-def readme():
-    with open('README.md') as f:
-        return f.read()
-
-
 scripts = glob.glob('bin/[a-z]*')
-
-setuptools.setup(
-    name='wikivector',
-    version='1.2.0',
-    description='Tools for encoding Wikipedia articles as vectors.',
-    long_description=readme(),
-    long_description_content_type="text/markdown",
-    author='Neal Morton',
-    author_email='mortonne@gmail.com',
-    license='GPLv3',
-    url='http://github.com/mortonne/wikivector',
-    packages=setuptools.find_packages('src'),
-    package_dir={'': 'src'},
-    scripts=scripts,
-    install_requires=[
-        'selectolax',
-        'numpy',
-        'h5py>=3',
-        'pandas',
-        'tensorflow_hub',
-    ],
-    classifiers=[
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-        'Programming Language :: Python :: 3.8',
-    ]
-)
+setuptools.setup(scripts=scripts)
